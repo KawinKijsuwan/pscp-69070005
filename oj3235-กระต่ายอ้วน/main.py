27ef68a209db67d2b96cb51fc87ec612339f263d
KawinKijsuwan <69070005@kmitl.ac.th>
2026-08-15 12:54:03 +0700

    1

---
+++ oj3235-กระต่ายอ้วน/main.py
@@ -0,0 +1,19 @@
+"""กระต่ายอ้วน"""
+n = int(input())
+rabbits_names = []
+rabbits_wieght = []
+for _ in range(n):
+    names,weight = input().split()
+    rabbits_names.append(names)
+    rabbits_wieght.append(int(weight))
+
+max_val = max(rabbits_wieght)
+max_index = rabbits_wieght.index(max_val)
+
+fat_rabbits = 0
+for i in range(n):
+    if int(rabbits_wieght[i]) > 15:
+        fat_rabbits += 1
+
+print(fat_rabbits)
+print(rabbits_names[max_index])
